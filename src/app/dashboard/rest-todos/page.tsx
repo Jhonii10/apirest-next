b8f64b52c0f7e7812611c445b6e24e135389e267
Jhonni10 <jhonieripianoscue@gmail.com>
2024-07-04 13:34:50 -0500

    revalided cache and updating prisma

--- src/app/dashboard/rest-todos/page.tsx
+++ src/app/dashboard/rest-todos/page.tsx
@@ -1,3 +1,7 @@
+export const dynamic = 'force-dynamic';
+export const revalidate = 0;
+
+
 import prisma from "@/lib/prisma";
 import { NewTodo } from "@/todos";
 import { TodosGrid } from "@/todos/components/TodosGrid";
